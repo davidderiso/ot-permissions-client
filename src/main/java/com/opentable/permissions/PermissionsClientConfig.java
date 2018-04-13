23716542b1748228448f44e29e1599585d1da676
Antonio Pellizzaro <apellizzaro@mbp15-apellizz-2901.local>
2018-04-12 18:07:46 -0700

    Added missing file

---
+++ src/main/java/com/opentable/permissions/PermissionsClientConfig.java
@@ -0,0 +1,28 @@
+package com.opentable.permissions;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonSerialize
+public class PermissionsClientConfig {
+
+
+    private String permissionServiceId;
+
+    private String permissionServiceUrl;
+
+    private String oauthServiceId;
+
+    private String oauthServiceUrl;
+
+    private String clientId;
+    private String clientSecret;
+}
